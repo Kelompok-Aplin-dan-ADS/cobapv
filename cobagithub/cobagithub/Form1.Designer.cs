fa07797679dec15cba5958714474640cc6eeee0b
Yongki <yongkitanu@gmail.com>
2019-09-03 13:26:28 +0700

    button anjing

--- cobagithub/cobagithub/Form1.Designer.cs
+++ cobagithub/cobagithub/Form1.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.anjing = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 13);
             this.button1.Location = new System.Drawing.Point(178, 119);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -51,11 +51,21 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // anjing
+            // 
+            this.anjing.Location = new System.Drawing.Point(30, 24);
+            this.anjing.Name = "anjing";
+            this.anjing.Size = new System.Drawing.Size(75, 23);
+            this.anjing.TabIndex = 2;
+            this.anjing.Text = "button3";
+            this.anjing.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.anjing);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,6 +78,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button anjing;
     }
 }
 
